38f0c8444ad787df3086afb0edb4b1bf37145057
julimorozova <yuli.sleptsova@gmail.com>
2021-07-15 17:19:09 +0300

    babel configuration is completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HTMLPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: ['@babel/polyfill', './src/index.js'],
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
@@ -11,13 +11,27 @@ module.exports = {
     },
     plugins: [
         new HTMLPlugin({
-            filename: "index.html",
-            template: "./src/index.html"
+            filename: 'index.html',
+            template: './src/index.html'
         })
     ],
     resolve: {
         extensions: [
             '.js'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
         ]
     }
 }
