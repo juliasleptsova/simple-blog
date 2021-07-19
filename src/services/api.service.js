ec513bf2ebd52d97976b3a905a6ac6d8e4dd8ec8
julimorozova <yuli.sleptsova@gmail.com>
2021-07-19 11:01:39 +0300

    add a api component and firebase

---
+++ src/services/api.service.js
@@ -0,0 +1,21 @@
+class ApiService {
+    constructor(baseUrl) {
+        this.url = baseUrl;
+    }
+
+    async createPost(post) {
+        try {
+            const request = new Request(this.url + '/posts.json', {
+                method: 'post',
+                body: JSON.stringify(post)
+            });
+            const response = await fetch(request);
+            return await response.json();
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export const apiService = new ApiService('https://js-blog-5b292-default-rtdb.firebaseio.com')
