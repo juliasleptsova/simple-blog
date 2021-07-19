84a078aceb070775ac1f01d6498719b9c03a2cac
julimorozova <yuli.sleptsova@gmail.com>
2021-07-19 12:02:40 +0300

    add a post component

--- src/core/component.js
+++ src/core/component.js
@@ -5,11 +5,17 @@ export class Component {
     }
     init() {}
 
+    onShow() {}
+
+    onHide() {}
+
     hide() {
         this.$el.classList.add('hide');
+        this.onHide();
     }
 
     show() {
         this.$el.classList.remove('hide');
+        this.onShow();
     }
 }
